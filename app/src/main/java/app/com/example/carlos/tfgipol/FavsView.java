43f8e074e9be7bae560732e61275b007ea0ebd72
CarlosEscobarBlasco <carlosescobarblasco@gmail.com>
2016-02-11 11:16:59 +0000

    New Interface and transition between the main view and favs View

---
+++ app/src/main/java/app/com/example/carlos/tfgipol/FavsView.java
@@ -0,0 +1,13 @@
+package app.com.example.carlos.tfgipol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favs_view);
+    }
+}
--- app/src/main/java/app/com/example/carlos/tfgipol/MainActivity.java
+++ app/src/main/java/app/com/example/carlos/tfgipol/MainActivity.java
@@ -1,13 +1,34 @@
 package app.com.example.carlos.tfgipol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button favsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activateButtons();
+    }
+
+    private void activateButtons() {
+        activateFavsButton();
+    }
+
+    private void activateFavsButton() {
+        favsButton = (Button) findViewById(R.id.favsButton);
+        favsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FavsView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
